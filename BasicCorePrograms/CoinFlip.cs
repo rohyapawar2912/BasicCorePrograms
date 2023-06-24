86c2da6778c0dd8f3cca791539029c46d022d955
“rohyapawar2912” <rohitpawar8530@gmail.com>
2023-06-24 16:21:42 +0530

    [Rohit]Add: // add all files

---
+++ BasicCorePrograms/CoinFlip.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicCorePrograms
+{
+    public class CoinFlip
+    {
+        private int numFlips;
+        private Random random;
+
+        public CoinFlip(int numFlips)
+        {
+            this.numFlips = numFlips;
+            random = new Random();
+        }
+
+        public void FlipCoin()
+        {
+            int headsCount = 0;
+            int tailsCount = 0;
+
+            for (int i = 0; i < numFlips; i++)
+            {
+                // Generate a random value between 0 and 1
+                double randomValue = random.NextDouble();
+
+                if (randomValue < 0.5)
+                {
+                    tailsCount++;
+                    Console.WriteLine("Tails");
+                }
+                else
+                {
+                    headsCount++;
+                    Console.WriteLine("Heads");
+                }
+            }
+
+            // Calculate the percentage of heads and tails
+            double headsPercentage = (double)headsCount / numFlips * 100;
+            double tailsPercentage = (double)tailsCount / numFlips * 100;
+
+            Console.WriteLine("Percentage of Heads: " + headsPercentage.ToString("0.00") + "%");
+            Console.WriteLine("Percentage of Tails: " + tailsPercentage.ToString("0.00") + "%");
+        }
+    }
+}
---
+++ BasicCorePrograms/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicCorePrograms
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the number of times to flip the coin:");
+            int numFlips = int.Parse(Console.ReadLine());
+
+            if (numFlips <= 0)
+            {
+                Console.WriteLine("Invalid input. Number of flips must be a positive integer.");
+                return;
+            }
+
+            CoinFlip coinFlip = new CoinFlip(numFlips);
+            coinFlip.FlipCoin();
+        }
+    }
+}
